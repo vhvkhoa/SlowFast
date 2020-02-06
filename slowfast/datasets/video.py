54df59c78221ad2ac735c0cb65572a1d6d29c1ce
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-02-07 01:26:47 +0900

    update

--- slowfast/datasets/video.py
+++ slowfast/datasets/video.py
@@ -84,6 +84,8 @@ class Video(torch.utils.data.Dataset):
             else:
                 fail_count += 1
         
+        video.release()
+        
         print('Failed to read %d. Read %d/%d frames' % (fail_count, len(self.frames), frames_length))
 
         self.frames = torch.as_tensor(np.stack(self.frames))
